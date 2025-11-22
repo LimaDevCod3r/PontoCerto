f9abd768e8f3580cf144859d0bb06981a6c55d70
Diego Lima <diegolima20100@gmail.com>
2025-11-22 18:26:36 -0300

    feat: Initial project setup with Spring Boot and database configuration
    Este commit inicializa o projeto PontoCerto.
    
    - Configuração do projeto Spring Boot com as seguintes dependências: Spring Data JPA, Spring Security, Spring Web, Spring Boot DevTools, Validation, MySQL Driver, Lombok
    
    - Adicionada configuração para conexão com banco de dados MySQL via application.yaml.
    
    - As credenciais do banco de dados foram externalizadas para variáveis de ambiente (DB_USERNAME e DB_PASSWORD) por segurança.

---
+++ src/test/java/github/com/LimaDevCod3r/PontoCertoApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.LimaDevCod3r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoCertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
