0ac1f00c9474b6cb4b148f748d50c78595484896
Diego Lima <diegolima20100@gmail.com>
2025-11-24 07:26:12 -0300

    feat(model): adiciona nível hierárquico à UserRole
    Refatora o enum `UserRole` para incluir um atributo `level` (nível hierárquico).
    
    - `SYSADMIN` (level 2)
    - `RH` (level 1)
    - `USER` (level 0)
    
    Esta mudança é a base para a implementação da lógica de controle de acesso hierárquico, permitindo que o `UserService` valide se um usuário tem permissão para criar outro com um nível de acesso inferior.

--- src/main/java/github/com/LimaDevCod3r/models/enums/UserRole.java
+++ src/main/java/github/com/LimaDevCod3r/models/enums/UserRole.java
@@ -4,14 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
-    USER("ROLE_USER"),
-    RH("ROLE_RH"),
-    SYSADMIN("ROLE_SYSADMIN");
+    USER("ROLE_USER",0),
+    RH("ROLE_RH",1),
+    SYSADMIN("ROLE_SYSADMIN",2);
 
     private final String role;
+    private final int level;
 
-    UserRole(String role) {
+    UserRole(String role, int level) {
         this.role = role;
+        this.level = level;
     }
 
 }
