f0e3b072e8eb86949626c0ede63ff87a51dacee3
Diego Lima <diegolima20100@gmail.com>
2025-11-24 07:19:25 -0300

    feat(repository): adiciona métodos de busca no UserRepository
    Adiciona novos métodos à interface UserRepository para suportar funcionalidades essenciais da aplicação.
    
    - **`findByUsername(String username)`**: Permite a busca de usuários pelo nome de usuário. Este método é crucial para a lógica de validação no UserService, garantindo que não sejam criados usuários com nomes duplicados.
    
    - existsByRole(UserRole role): Verifica de forma eficiente se existe pelo menos um usuário com uma determinada UserRole. Este método é utilizado pelo DataInitializer para evitar a criação de múltiplos usuários SYSADMIN na inicialização da aplicação.

--- src/main/java/github/com/LimaDevCod3r/repositories/UserRepository.java
+++ src/main/java/github/com/LimaDevCod3r/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package github.com.LimaDevCod3r.repositories;
 
 import github.com.LimaDevCod3r.models.User;
+import github.com.LimaDevCod3r.models.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByRole(UserRole role);
 }
